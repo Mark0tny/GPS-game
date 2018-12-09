0952b2de1d8e8ea46e3434876df036e975758ab7
Mark0tny <marcinkotulski95@gmail.com>
2018-12-09 18:41:07 +0100

    add login and registration layouts

--- app/src/main/java/com/example/kotu9/gpsgame/UserProfileActivity.java
+++ app/src/main/java/com/example/kotu9/gpsgame/UserProfileActivity.java
@@ -3,11 +3,11 @@ package com.example.kotu9.gpsgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class UserProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_user_profile);
     }
 }
