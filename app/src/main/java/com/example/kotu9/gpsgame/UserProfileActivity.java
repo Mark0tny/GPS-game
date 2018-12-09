7e4bfe8aa76a9840edbc62362424b33837f71d29
Mark0tny <marcinkotulski95@gmail.com>
2018-12-09 22:15:30 +0100

    Profile Image upload

--- app/src/main/java/com/example/kotu9/gpsgame/UserProfileActivity.java
+++ app/src/main/java/com/example/kotu9/gpsgame/UserProfileActivity.java
@@ -1,40 +1,143 @@
 package com.example.kotu9.gpsgame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    EditText editText;
-    Button savebtn;
+    private static final int CHOOSE_IMAGE = 101;
+    private ImageView imageView;
+    private EditText editText;
+    private Button savebtn;
+    private ProgressBar progressBar;
+    Uri uriProfileImage;
+    String profileImageUrl;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-
+        mAuth = FirebaseAuth.getInstance();
         imageView = findViewById(R.id.imageView);
         editText = findViewById(R.id.editTextDisplayName);
         savebtn = findViewById(R.id.buttonSave);
+        progressBar = findViewById(R.id.progressbarImage);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                showImageChooser();
             }
         });
+
         savebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                saveUserInformation();
             }
         });
-
-
     }
+
+    private void saveUserInformation() {
+        String dissplayName = editText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(dissplayName)) {
+            editText.setError("Please enter username");
+            editText.requestFocus();
+            return;
+        }
+        FirebaseUser user =  mAuth.getCurrentUser();
+        if(user != null && profileImageUrl != null){
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(dissplayName)
+                    .setPhotoUri(Uri.parse(profileImageUrl))
+                    .build();
+            user.updateProfile(profile).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(),"Profile updated",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(),task.getException().toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uriProfileImage = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriProfileImage);
+                imageView.setImageBitmap(bitmap);
+                uploadImageToFirebaseStorage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImageToFirebaseStorage() {
+        StorageReference profileImageURI =
+                FirebaseStorage.getInstance().getReference("profileimages/" + System.currentTimeMillis() + ".jpg");
+
+        if (uriProfileImage != null) {
+            progressBar.setVisibility(View.VISIBLE);
+            profileImageURI.putFile(uriProfileImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressBar.setVisibility(View.GONE);
+                    profileImageUrl = taskSnapshot.getStorage().getDownloadUrl().toString();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void showImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), CHOOSE_IMAGE);
+    }
+
 }
