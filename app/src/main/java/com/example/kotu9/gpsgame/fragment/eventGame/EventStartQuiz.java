c9e6655181996f2582174e71122a32cb989c93c8
Mark0tny <marcinkotulski95@gmail.com>
2019-09-01 23:04:22 +0200

    quiz layout + init view

--- app/src/main/java/com/example/kotu9/gpsgame/fragment/eventGame/EventStartQuiz.java
+++ app/src/main/java/com/example/kotu9/gpsgame/fragment/eventGame/EventStartQuiz.java
@@ -1,19 +1,58 @@
 package com.example.kotu9.gpsgame.fragment.eventGame;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.kotu9.gpsgame.R;
+import com.example.kotu9.gpsgame.model.ClusterMarker;
+import com.example.kotu9.gpsgame.model.Event;
+import com.example.kotu9.gpsgame.model.Question;
+import com.example.kotu9.gpsgame.model.QuizType;
 
-public class EventStartQuiz extends Fragment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EventStartQuiz extends Fragment implements View.OnClickListener {
+
+    final static long INTERVAL = 1000;
+    final static long TIMEOUT = 1000;
+    int progressValue = 0;
+
+    private CountDownTimer countDownTimer;
+
+    int index, thisQuestion, totalQuestion, correctAnswer, score;
+
+
+    //View
+    private ProgressBar progressBar;
+    private Button btnA, btnB, btnC, btnD;
+    private TextView mQuestionValue, mQuestionNum, mScore;
+    private NavController navController;
+
+
+    //Data
+    private ClusterMarker clusterMarker;
+    private Event event;
+    private QuizType quizType;
+    private List<Question> questionList;
+    private Map<String, Boolean> answersMap = new HashMap<>();
+    private List<TextView> answers = new ArrayList<>();
 
     public EventStartQuiz() {
-        // Required empty public constructor
-    }
 
+    }
 
     public static EventStartQuiz newInstance() {
         EventStartQuiz fragment = new EventStartQuiz();
@@ -27,7 +66,16 @@ public class EventStartQuiz extends Fragment {
         getActivity().setTitle(R.string.fr_start_quiz);
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            clusterMarker = new ClusterMarker();
+            clusterMarker = (ClusterMarker) getArguments().get(String.valueOf(R.string.markerBundleGame));
 
+//            event = new QuizType();
+//            event = clusterMarker.getEvent();
+//
+//            quizType = new QuizType(event);
+//
+//            questionList = new ArrayList<>();
+//            questionList = quizType.questionsList;
         }
     }
 
@@ -36,7 +84,70 @@ public class EventStartQuiz extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_event_start_quiz, container, false);
+
+
         return view;
     }
 
+    private void setupViews(View view) {
+        progressBar = view.findViewById(R.id.progressBarQuiz);
+
+        btnA = view.findViewById(R.id.btnAnswer1);
+        btnB = view.findViewById(R.id.btnAnswer2);
+        btnC = view.findViewById(R.id.btnAnswer3);
+        btnD = view.findViewById(R.id.btnAnswer4);
+
+        mQuestionValue = view.findViewById(R.id.question_text);
+        mQuestionNum = view.findViewById(R.id.question_size);
+        mScore = view.findViewById(R.id.question_points);
+
+
+        navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_start_game);
+
+        btnA.setOnClickListener(this);
+        btnB.setOnClickListener(this);
+        btnC.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        countDownTimer.cancel();
+        if (index < questionList.size()) {
+            Button clickedBytton = (Button) view;
+            if (true) { //warunek gdzy pytanie ok pobranie z mapy on bierze string
+                correctAnswer++;
+                score += 10;
+                showQuestion(++index);
+            } else {
+                // coś gdzy zle odpowie
+                // kolorować buttonki przed zmiana na i go next
+            }
+
+            mScore.setText(String.format("%d", score));
+        }
+    }
+
+    private void showQuestion(int index) {
+
+        if (index < questionList.size()) {
+
+            thisQuestion++;
+            mQuestionNum.setText(String.format("%d/%d", thisQuestion, questionList.size()));
+            progressBar.setProgress(0);
+            progressValue = 0;
+
+            answersMap = questionList.get(index).getAnswers();
+            mQuestionValue.setText(questionList.get(index).getQuestion());
+            int answersPosition = 0;
+            for (Map.Entry<String, Boolean> entry : answersMap.entrySet()) {
+                answers.get(answersPosition).setText(entry.getKey());
+                answersPosition++;
+            }
+
+        }
+
+
+    }
 }
